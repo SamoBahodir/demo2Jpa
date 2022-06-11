6dcb7b1bb5f51edd7909bfc4077b50d4ccce0a82
Bahodir <yahda.seven@gmail.com>
2022-06-11 16:42:29 +0500

    add

---
+++ src/main/java/com/example/demo2jpa/Demo2JpaApplicaton.java
@@ -0,0 +1,13 @@
+package com.example.demo2jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2JpaApplicaton {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2JpaApplicaton.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo2jpa/Demo2JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo2jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2JpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
